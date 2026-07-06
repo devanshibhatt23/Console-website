73429710d449f04f6f9f8f24275ec3779e593b6f
Ridhima Garg <coderidhimagarg0915@gmail.com>
2026-07-06 18:46:49 +0530

    Setup Supabase client and environment configuration

---
+++ client/src/lib/supabase.js
@@ -0,0 +1,10 @@
+import { createClient } from "@supabase/supabase-js";
+
+console.log(import.meta.env);
+console.log(import.meta.env.VITE_SUPABASE_URL);
+console.log(import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
+
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
+);
